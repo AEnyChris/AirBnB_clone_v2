dd631599ad946b5d3a449bc760145b10a49b0985
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-06-30 01:57:20 +0100

    added task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+import os
 
+class Amenity(BaseModel, Base):
+    """Creates an amenity object"""
+    __tablename__ = 'amenities'
 
-class Amenity(BaseModel):
-    name = ""
+    name = Column(
+            String(128), nullable=False
+            ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ""
